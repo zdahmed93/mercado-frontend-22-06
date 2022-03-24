194893eee5d419923f3b068bbeb5191e0ec8603d
DevMasteryLabs <develop.devmastery@gmail.com>
2022-03-24 23:52:33 +0100

    Set up react-router-dom@5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import Button from 'react-bootstrap/Button'
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+import CreateItem from './pages/CreateItem';
+import Home from './pages/Home';
+import ItemDetails from './pages/ItemDetails';
+import Items from './pages/Items';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import UpdateItem from './pages/UpdateItem';
 
 function App() {
   return (
-    <div className='container mt-5'>
-      <h1>Hello DevMastery</h1>
-      <Button>OK</Button>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/profile' component={Profile} />
+        <Route path='/items' component={Items} />
+        <Route path='/create-item' component={CreateItem} />
+        <Route path='/update-item/:id' component={UpdateItem} />
+        <Route path='/items/:id' component={ItemDetails} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
