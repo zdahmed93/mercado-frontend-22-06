e3f16f13c94a2ef2b9d446f28baabd695d42f226
DevMasteryLabs <develop.devmastery@gmail.com>
2022-03-25 04:40:02 +0100

    Get user informations from redux and display them in the Profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Profile() {
+  const userInfo = useSelector(state => state.user.info)
   return (
-    <div>Profile</div>
+    <div className='m-5'>
+      <h1 className='text-center'>Profile</h1>
+      <h2> {userInfo.firstName} </h2>
+      <h2> {userInfo.lastName} </h2>
+      <h4> {userInfo.email} </h4>
+    </div>
   )
 }
 
