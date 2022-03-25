7accac4cfb7df8730e6e51ec59089cc8bc5ecda9
DevMasteryLabs <develop.devmastery@gmail.com>
2022-03-26 00:45:22 +0100

    Install the "cdbreact" package and import Sidebar component to use it in the InsideLayout component

--- src/layouts/InsideLayout.js
+++ src/layouts/InsideLayout.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
+import Sidebar from '../components/Sidebar'
+
 function InsideLayout(props) {
-    return (
-        <div style={{ display: 'flex' }}>
-            <div style={{ backgroundColor: 'lightblue', padding: 20, height: '100vh' }}>
-                <p>InsideLayout Sidebar</p>
-            </div>
-            <div>
-                {props.children}
-            </div>
+  return (
+    <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div>
+            {props.children}
         </div>
-    )
+    </div>
+  )
 }
 
 export default InsideLayout
