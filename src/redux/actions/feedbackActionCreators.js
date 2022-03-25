bac36bc2c116ea310a4c997f19b84e74a0af3692
DevMasteryLabs <develop.devmastery@gmail.com>
2022-03-25 04:17:18 +0100

    DRY: Handle the request error toast inside the 'requestFailed' action creator

--- src/redux/actions/feedbackActionCreators.js
+++ src/redux/actions/feedbackActionCreators.js
@@ -9,7 +9,14 @@ export const requestSucceeded = () => ({
     type: REQUEST_SUCCEEDED
 })
 
-export const requestFailed = (errorMessage) => {
+export const requestFailed = (err) => {
+    let errorMessage = err.message || 'Request failed'
+    if (err && err.response && err.response.data && err.response.data.error && typeof(err.response.data.error) === 'string') {
+        errorMessage = err.response.data.error
+    }
+    if (err && err.response && err.response.data && err.response.data.error && err.response.data.error.details) {
+        errorMessage =  err.response.data.error.details[0] && err.response.data.error.details[0].message
+    }
     if (errorMessage) {
         alertError(errorMessage)
     }
