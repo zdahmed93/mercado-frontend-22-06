302eb23faa0e2d548a0021418eccab1e9453c8fa
DevMasteryLabs <develop.devmastery@gmail.com>
2022-03-26 02:41:05 +0100

    Display items in the Home page

---
+++ src/components/DisplayItems.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import { Container, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { fetchAllItems } from '../redux/actions/itemsActionCreators'
+import ItemCard from './ItemCard'
+
+function DisplayItems() {
+    const items = useSelector(state => state.items.all)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchAllItems())
+    }, [])
+    return (
+        <Container>
+            <Row>
+                {items.map(item => (
+                    <ItemCard item={item} />
+                ))}
+            </Row>
+        </Container>
+    )
+}
+
+export default DisplayItems
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,10 @@
-import React, { useEffect } from 'react'
-import { Container, Row } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
 
-import ItemCard from '../components/ItemCard'
-
-import { fetchAllItems } from '../redux/actions/itemsActionCreators'
+import DisplayItems from '../components/DisplayItems'
 
 function Home() {
-    const items = useSelector(state => state.items.all)
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(fetchAllItems())
-    }, [])
   return (
-      <Container>
-          <Row >
-            {items.map(item => (
-                <ItemCard item={item} />
-            ))}
-          </Row>
-      </Container>
+    <DisplayItems />
   )
 }
 
