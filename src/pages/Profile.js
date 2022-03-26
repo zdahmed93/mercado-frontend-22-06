5fba2b867cebbdfa93f446fbbfd116d1ecb4a295
DevMasteryLabs <develop.devmastery@gmail.com>
2022-03-26 04:01:54 +0100

    Chane Profile page content

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import Table from 'react-bootstrap/Table'
 
 function Profile() {
   const userInfo = useSelector(state => state.user.info)
   return (
-    <div className='m-5'>
+    <>
       <h1 className='text-center'>Profile</h1>
-      <h2> {userInfo.firstName} </h2>
-      <h2> {userInfo.lastName} </h2>
-      <h4> {userInfo.email} </h4>
-    </div>
+      <Table striped bordered hover style={{ maxWidth: 500 }} className="mx-auto">
+        <tbody>
+          <tr>
+            <td>First Name</td>
+            <td> {userInfo.firstName} </td>
+          </tr>
+          <tr>
+            <td>Last Name</td>
+            <td> {userInfo.lastName} </td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td> {userInfo.email} </td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
   )
 }
 
